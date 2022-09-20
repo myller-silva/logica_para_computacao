f4b51b119f1de610c06a98dc3a1e6661b4652cd1
Myller Silva <myllersilva1310@gmail.com>
2022-09-20 18:15:57 -0300

    Create q01.py

---
+++ LogiComp/q01.py
@@ -0,0 +1,22 @@
+from formula import *
+from functions import *
+
+def number_of_connectives(A):
+  if(isinstance(A, Atom)):
+    return 1;
+  if(isinstance(A, Not)):
+    return 1 + number_of_connectives(A.inner);
+  if(isinstance(A, Or) or isinstance(A, And) or isinstance(A, Implies)):
+    return 1 + number_of_connectives(A.left) + number_of_connectives(A.right);
+
+
+p = Atom('p');
+q = Atom('q');
+r = Atom('r');
+
+fol = Implies(p, q)
+print(fol)
+print(f'number_of_connectives: {number_of_connectives(fol)}')
+
+
+
