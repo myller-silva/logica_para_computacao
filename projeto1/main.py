c6439a386374aad7ddfa0ccfac1db135aacfba1d
Myller Silva <myllersilva1310@gmail.com>
2022-10-16 12:22:09 -0300

    update LogiComp

--- projeto1/main.py
+++ projeto1/main.py
@@ -6,6 +6,9 @@ def satisfiablity_checking(formula):
 def atoms():
   pass
 
+def UniaoDeConjuntos(interpretacao, interpretacao2):
+  intercecao = None
+  return interpretacao + interpretacao2 - intercecao
 
 def sat(formula, atoms, interpretation):
   if(len(atoms)==0):
@@ -15,14 +18,17 @@ def sat(formula, atoms, interpretation):
       False 
     
   atom = atoms.pop()
-  interpretation1 = interpretation U {atom, True}
-  interpretation2 = interpretation U {atom, False}
+  interpretation1 = UniaoDeConjuntos(interpretation, {atom, True})
+  interpretation2 = UniaoDeConjuntos(interpretation, {atom, False})
   result = sat(formula, atoms, interpretation1)
-  if(result!= False){
+  if(result!= False):
     return result
-  }
   return sat(formula, atoms, interpretation2)
   
 
 def truth_value():
   pass
+
+
+from LogiComp.formula import Atom, And
+print(And('p', Atom('q')))
